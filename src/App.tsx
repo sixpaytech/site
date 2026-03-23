29938bae9d4c5f7569b12c26eea6c4a17890437b
emersonrf227 <emersonrf227@yahoo.com.br>
2026-03-23 21:07:48 +0000

    terms ok

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import PrivacyPolicy from "./pages/privacy_police";
-import Useterms from "./pages/use_terms";
+import PrivacyPolicy from "./pages/privacy_police/Index";
+import Useterms from "./pages/use_terms/Index";
 
 import NotFound from "./pages/NotFound";
 
