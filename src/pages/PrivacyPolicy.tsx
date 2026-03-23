9976d476f68470ca32b9cfeefde4f027ca9a383b
emersonrf227 <emersonrf227@yahoo.com.br>
2026-03-23 21:31:58 +0000

    ok agora

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -2,16 +2,13 @@ import Footer from "@/components/Footer";
 
 const PrivacyPolicy = () => {
   return (
-    <div>
-      <div className="min-h-screen">
-        <div className="w-full h-[800px]">
-          {/* <iframe
-            src="/Política_de_Privacidade_6Pay.pdf"
-            className="w-full h-full"
-          /> */}
-        </div>
+    <div className="min-h-screen">
+      <div className="w-full h-[800px]">
+        <iframe
+          src="/Política_de_Privacidade_6Pay.pdf"
+          className="w-full h-full"
+        />
       </div>
-      <Footer />
     </div>
   );
 };
