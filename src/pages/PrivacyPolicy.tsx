11b6ded53d7b5d470bf18b5ac6c0e88bbfde7bc4
emersonrf227 <emersonrf227@yahoo.com.br>
2026-03-23 21:25:21 +0000

    privacy polyci

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -1,12 +1,17 @@
+import Footer from "@/components/Footer";
+
 const PrivacyPolicy = () => {
   return (
-    <div className="min-h-screen">
-      <div className="w-full h-[800px]">
-        <iframe
-          src="/Política_de_Privacidade_6Pay.pdf"
-          className="w-full h-full"
-        />
+    <div>
+      <div className="min-h-screen">
+        <div className="w-full h-[800px]">
+          {/* <iframe
+            src="/Política_de_Privacidade_6Pay.pdf"
+            className="w-full h-full"
+          /> */}
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
